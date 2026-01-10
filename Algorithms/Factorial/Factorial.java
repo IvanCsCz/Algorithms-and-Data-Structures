3ea42f1308fa911aaab9ec31a5bbbc589b79a310
Jose Ivan Campos Chavez <ivan820plus@hotmail.com>
2026-01-09 23:20:06 -0600

    Add Factorial algorithm in Java

---
+++ Algorithms/Factorial/Factorial.java
@@ -0,0 +1,11 @@
+package Algorithms.Factorial;
+
+public class Factorial {
+  public static int factorial(int num) {
+    int result = 1;
+    for (int i = 1; i <= num; i++) {
+      result *= i;
+    }
+    return result;
+  }
+}
